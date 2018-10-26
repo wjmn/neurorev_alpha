112af4b36a065da8caa65d4329293f987427a0bb
wjmn <Jamin.Wu07@gmail.com>
2018-10-26 21:52:12 +1100

    Updates

---
+++ precache-manifest.da135768e4da4aa7c604e42fc180698f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fddd5b9d33d2f6a11ac",
+    "url": "/neurorev_alpha/static/js/runtime~main.0fddd5b9.js"
+  },
+  {
+    "revision": "b82ded8d5af686546fdb",
+    "url": "/neurorev_alpha/static/js/main.b82ded8d.chunk.js"
+  },
+  {
+    "revision": "6d132d0c230b4dffc359",
+    "url": "/neurorev_alpha/static/js/1.6d132d0c.chunk.js"
+  },
+  {
+    "revision": "b82ded8d5af686546fdb",
+    "url": "/neurorev_alpha/static/css/main.47a8c2a0.chunk.css"
+  },
+  {
+    "revision": "6d132d0c230b4dffc359",
+    "url": "/neurorev_alpha/static/css/1.55dbccc4.chunk.css"
+  },
+  {
+    "revision": "e47e05fd6647d1d8840c7520e68cc9f8",
+    "url": "/neurorev_alpha/index.html"
+  }
+];
